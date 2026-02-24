58a4228d8fee9b96cb195220321052e75dc204ed
Youcef Samir <syoucef@MacBook-Pro-de-Youcef.local>
2026-02-24 10:43:37 +0100

    Fin de l'API sans DAO

--- index.php
+++ index.php
@@ -3,9 +3,21 @@
 require_once 'config/config.php';
 require_once 'services/StudentService.php';
 
-// Récupération des données
+/* Gestion du POST */
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    $prenom = $_POST['prenom'];
+    $age = $_POST['age'];
+
+    StudentService::addStudent($prenom, $age);
+
+    // Redirection anti-doublon (PRG pattern)
+    header('Location: index.php');
+    exit;
+}
+
+/* Récupération des données */
 $students = StudentService::getAllStudents();
 
-// Affichage
+/* Affichage */
 require_once 'views/students.php';
 
--- services/StudentService.php
+++ services/StudentService.php
@@ -9,5 +9,29 @@ class StudentService
 
         return json_decode($response, true);
     }
+
+
+    public static function addStudent($prenom, $age)
+{
+    $url = API_BASE_URL . '/students';
+
+    $data = [
+        'prenom' => $prenom, // L'API attend 'name'
+        'age' => $age
+    ];
+
+    $options = [
+        'http' => [
+            'header'  => "Content-Type: application/json\r\n",
+            'method'  => 'POST',
+            'content' => json_encode($data)
+        ]
+    ];
+
+    $context = stream_context_create($options);
+
+    file_get_contents($url, false, $context);
+}
+
 }
 
